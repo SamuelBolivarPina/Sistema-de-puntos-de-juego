a4dd89668e4dd3dbfae72a0efcdacdee2f1e893e
Merari200527 <mbotellom@unicartagena.edu.co>
2026-06-09 21:26:52 -0500

    se realizo el menu

---
+++ menu.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int opcion;
+        do{
+        System.out.println("=== Sistema de Puntos de Juego ===\n" +
+                "\n" +
+                "1. Calcular puntos por victoria\n" +
+                "2. Calcular puntos por empate\n" +
+                "3. Calcular penalización\n" +
+                "4. Clasificar nivel del jugador\n" +
+                "5. Salir\n");
+        System.out.println("Ingrese la opcion que desea:");
+
+        while (!teclado.hasNextInt()) {
+            System.out.println("Por favor, ingresa un número válido del 1 al 5:");
+            teclado.next();
+        }
+
+        opcion = teclado.nextInt();
+        switch (opcion) {
+            case 1:
+                break;
+
+            case 2:
+
+
+                break;
+            case 3:
+
+
+                break;
+            case 4:
+
+
+                break;
+            case 5:
+                break;
+            default:
+                System.out.println("el numero que ingreso no esta en el menu");
+
+        }
+
+    } while(opcion !=5);
+}
+
+public static double pedirNumero(Scanner teclado, String mensaje) {
+    System.out.println(mensaje);
+    while (!teclado.hasNextDouble()) {
+        System.out.println("Error: ¡Eso no es un número! Inténtalo de nuevo:");
+        teclado.next(); // Limpia la letra de la memoria
+    }
+    return teclado.nextDouble();
+}
+
+}
