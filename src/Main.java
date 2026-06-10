2e2938dd9871e6eb37c487c06352fdd9d56bf563
Paolo Trujillo Artunduaga <ptrujilloa@unicartagena.edu.co>
2026-06-09 23:23:36 -0500

    Clase PuntosEmpate

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int puntajeFinal = PuntosEmpate.calcularPuntosEmpate();
+
+        System.out.println("\nPuntaje retornado al sistema: " + puntajeFinal);
+
+    }
+}
